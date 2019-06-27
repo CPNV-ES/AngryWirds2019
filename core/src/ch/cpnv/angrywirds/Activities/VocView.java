192d16b7d426816fa125283e96517a0eff9a10d1
QuentinRo <quentin.rossier@cpnv.ch>
2019-06-27 16:56:58 +0200

    add if marked, don't show

--- core/src/ch/cpnv/angrywirds/Activities/VocView.java
+++ core/src/ch/cpnv/angrywirds/Activities/VocView.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector3;
 
 import java.util.ArrayList;
 
@@ -16,10 +17,14 @@ import ch.cpnv.angrywirds.Models.Stage.Scenery;
 import ch.cpnv.angrywirds.Models.Stage.Title;
 import ch.cpnv.angrywirds.Providers.VocProvider;
 
-public class VocView extends GameActivity {
+public class VocView extends GameActivity implements InputProcessor{
     private Texture background;
     private Title title;
     private DisplayWord wordtoshow;
+    private DisplayWord wordlanguage2;
+
+    private Sprite backbutton;
+
     private int h;
     private int l;
     private Vocabulary vocabulary; // The vocabulary we train
@@ -31,6 +36,10 @@ public class VocView extends GameActivity {
         background = new Texture(Gdx.files.internal("background.png"));
         // title = new Title("Vocabulaire");
         // vocabulary = ;
+
+        backbutton = new Sprite(new Texture("backbutton.png"));
+        backbutton.setBounds(500, 0,100,100);
+        backbutton.setOrigin(50,50);
 
         Gdx.app.log("HAHA",  vocabulary.getWords()+ "  voc ! ");
 
@@ -48,28 +57,38 @@ public class VocView extends GameActivity {
         {
             // currentword = word.getValue1();
 
-            wordtoshow = new DisplayWord(word.getValue1(), 100, 900-h);
+            if (word.marked())
+            {
+             return;
+            }else {
+                wordtoshow = new DisplayWord(word.getValue1(), 100, 900-h);
+                wordlanguage2 =  new DisplayWord(word.getValue2(), 400, 900-h);
 
-            try {
-                scenery.addDisplayWord(wordtoshow);
-                Gdx.app.log("ANGRY", "Tout est bon");
-            } catch (Exception e) {
-                e.printStackTrace();
-                Gdx.app.log("ANGRY", "Could not add block to scenery");
+
+
+                try {
+                    scenery.addDisplayWord(wordtoshow);
+                    scenery.addDisplayWord(wordlanguage2);
+                    Gdx.app.log("ANGRY", "Tout est bon");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Gdx.app.log("ANGRY", "Could not add block to scenery");
+                }
+
+                h += 32;
+                Gdx.app.log("HAHA",  wordtoshow+ "  MOtus ! ");
             }
 
-            h += 50;
-            Gdx.app.log("HAHA",  wordtoshow+ "  MOtus ! ");
 
         }
-
+        Gdx.input.setInputProcessor(this);
         spriteBatch.begin();
         spriteBatch.draw(background, 0, 0, camera.viewportWidth, camera.viewportHeight);
         // title.draw(spriteBatch);
         // wordtoshow.draw(spriteBatch);
         scenery.drawWord(spriteBatch);
+        backbutton.draw(spriteBatch);
         spriteBatch.end();
-
     }
 
     @Override
@@ -88,13 +107,52 @@ public class VocView extends GameActivity {
 
     }
 
+    @Override
     public boolean keyDown(int keycode) {
-    if(keycode == 50)
-    {
-        AngryWirds.gameActivityManager.pop(); // play
-    }
+
 
         return false;
     }
 
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        Vector3 pointTouched = camera.unproject(new Vector3(screenX, screenY, 0)); // Convert from screen coordinates to camera coordinates
+        Gdx.app.log("HAHA",  "  Touched  ! ");
+        if (pointTouched.x >= 500 && pointTouched.x <= 560 && pointTouched.y >= 0 && pointTouched.y <= 60 )
+        {
+            Gdx.app.log("HAHA",  "  Pop vocview");
+            AngryWirds.gameActivityManager.pop();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
 }
